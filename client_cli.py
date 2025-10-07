6729759243cffc0511e7df5df9b3df2097d60d3f
FrosT2k5 <yashpatil2k5@gmail.com>
2025-10-07 21:35:03 +0530

    client_cli: [EXPERIMENTAL] resolve the hosted instance ip
    Signed-off-by: FrosT2k5 <yashpatil2k5@gmail.com>

--- client_cli.py
+++ client_cli.py
@@ -1,6 +1,10 @@
 from exposehost.client import Client
+import socket
+
+server_host = "exposehost.southindia.cloudapp.azure.com"
+server_ip = socket.gethostbyname(server_host)
 
 port = input("Port to forward: ")
 subdomain = input("Subdomain: ")
-client = Client('127.0.0.1', port, '127.0.0.1', 1435, 'http', subdomain)
+client = Client('127.0.0.1', port, server_ip, 1435, 'http', subdomain)
 client.start()
